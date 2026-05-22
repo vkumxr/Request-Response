2b0fa41729b53431fd036e142f491c4c027090f4
Vishwa <vishwakumarv05@gmail.com>
2026-05-22 13:05:09 +0530

    Update TestResult.cs

--- TestResult.cs
+++ TestResult.cs
@@ -1,27 +1,35 @@
+using System;
+
 namespace Edj20Tester.Models
 {
     public enum ModbusFunction
     {
-        //Read
-        FC01_ReadCoils = 1,
-        FC02_ReadDiscreteInputs = 2,
         FC03_ReadHoldingRegisters = 3,
         FC04_ReadInputRegisters = 4,
-
-        //Write
-        FC05_WriteSingleCoil = 5,
         FC06_WriteSingleRegister = 6,
-        FC15_WriteMultipleCoils = 15,
         FC16_WriteMultipleRegisters = 16,
+    }
+
+    public enum TestStatus
+    {
+        Pass,
+        Fail,
+        Error,
+        Skipped
     }
 
     public class TestResult
     {
         public string TestPoint { get; set; }
         public string TestType { get; set; }
+        public ModbusFunction Function { get; set; }
         public double MeasuredValue { get; set; }
         public string Unit { get; set; }
         public string ExpectedRange { get; set; }
-        public string Result { get; set; }
+        public TestStatus Status { get; set; }
+        public string Notes { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+        public bool IsPass => Status == TestStatus.Pass;
     }
 }
